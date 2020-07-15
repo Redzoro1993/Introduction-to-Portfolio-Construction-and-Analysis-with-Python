9a781c5f665f41928882d254bc61e4f3c01ddbc1
Redzoro1993 <63926632+Redzoro1993@users.noreply.github.com>
2020-07-14 22:31:40 -0500

    Update edhec_risk_kit_104.py

--- Modules/edhec_risk_kit_104.py
+++ Modules/edhec_risk_kit_104.py
@@ -18,7 +18,7 @@ def get_ffme_returns():
     """
     Load the Fama-French Dataset for the returns of the Top and Bottom Deciles by MarketCap
     """
-    me_m = pd.read_csv("C:/Users/dfg_9/Documents/David/Coursera/Introduction to Portfolio Construction and Analysis with Python/data/Portfolios_Formed_on_ME_monthly_EW.csv",
+    me_m = pd.read_csv("https://raw.githubusercontent.com/Redzoro1993/Introduction-to-Portfolio-Construction-and-Analysis-with-Python/master/Data/Portfolios_Formed_on_ME_monthly_EW.csv",
                        header=0, index_col=0, na_values=-99.99)
     rets = me_m[['Lo 10', 'Hi 10']]
     rets.columns = ['SmallCap', 'LargeCap']
