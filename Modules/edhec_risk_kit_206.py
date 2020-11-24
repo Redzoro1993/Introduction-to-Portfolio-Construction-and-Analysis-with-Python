e6e53b74897708ac0da017b0d1639b80fa7a496a
Redzoro1993 <63926632+Redzoro1993@users.noreply.github.com>
2020-11-24 03:37:03 -0500

    Update edhec_risk_kit_206.py

--- Modules/edhec_risk_kit_206.py
+++ Modules/edhec_risk_kit_206.py
@@ -33,15 +33,15 @@ def get_hfi_returns():
     hfi.index = hfi.index.to_period('M')
     return hfi
 
-def get_ind_file(filetype, weighting="vw", n_inds=30):
+def get_ind_file(filetype):
     """
-    Load and format the Ken French Industry Portfolios files
-    Variant is a tuple of (weighting, size) where:
-        weighting is one of "ew", "vw"
-        number of inds is 30 or 49
-    """    
+    Load and format the Ken French 30 Industry Portfolios files
+    """
+    known_types = ["returns", "nfirms", "size"]
+    if filetype not in known_types:
+        raise ValueError(f"filetype must be one of:{','.join(known_types)}")
     if filetype is "returns":
-        name = f"{weighting}_rets" 
+        name = "vw_rets"
         divisor = 100
     elif filetype is "nfirms":
         name = "nfirms"
@@ -49,53 +49,42 @@ def get_ind_file(filetype, weighting="vw", n_inds=30):
     elif filetype is "size":
         name = "size"
         divisor = 1
-    else:
-        raise ValueError(f"filetype must be one of: returns, nfirms, size")
-    
-    ind = pd.read_csv(f"data/ind{n_inds}_m_{name}.csv", header=0, index_col=0, na_values=-99.99)/divisor
+                         
+    ind = pd.read_csv(f"https://raw.githubusercontent.com/Redzoro1993/Introduction-to-Portfolio-Construction-and-Analysis-with-Python/master/Data/ind30_m_{name}.csv", header=0, index_col=0)/divisor
     ind.index = pd.to_datetime(ind.index, format="%Y%m").to_period('M')
     ind.columns = ind.columns.str.strip()
     return ind
 
-def get_ind_returns(weighting="vw", n_inds=30):
+def get_ind_returns():
     """
-    Load and format the Ken French Industry Portfolios Monthly Returns
+    Load and format the Ken French 30 Industry Portfolios Value Weighted Monthly Returns
     """
-    return get_ind_file("returns", weighting=weighting, n_inds=n_inds)
+    return get_ind_file("returns")
 
-def get_ind_nfirms(n_inds=30):
+def get_ind_nfirms():
     """
     Load and format the Ken French 30 Industry Portfolios Average number of Firms
     """
-    return get_ind_file("nfirms", n_inds=n_inds)
+    return get_ind_file("nfirms")
 
-def get_ind_size(n_inds=30):
+def get_ind_size():
     """
     Load and format the Ken French 30 Industry Portfolios Average size (market cap)
     """
-    return get_ind_file("size", n_inds=n_inds)
+    return get_ind_file("size")
 
-
-def get_ind_market_caps(n_inds=30, weights=False):
-    """
-    Load the industry portfolio data and derive the market caps
-    """
-    ind_nfirms = get_ind_nfirms(n_inds=n_inds)
-    ind_size = get_ind_size(n_inds=n_inds)
-    ind_mktcap = ind_nfirms * ind_size
-    if weights:
-        total_mktcap = ind_mktcap.sum(axis=1)
-        ind_capweight = ind_mktcap.divide(total_mktcap, axis="rows")
-        return ind_capweight
-    #else
-    return ind_mktcap
-
-def get_total_market_index_returns(n_inds=30):
+                         
+def get_total_market_index_returns():
     """
     Load the 30 industry portfolio data and derive the returns of a capweighted total market index
     """
-    ind_capweight = get_ind_market_caps(n_inds=n_inds)
-    ind_return = get_ind_returns(weighting="vw", n_inds=n_inds)
+   
+    ind_nfirms = get_ind_nfirms()
+    ind_size = get_ind_size()
+    ind_return = get_ind_returns()
+    ind_mktcap = ind_nfirms * ind_size
+    total_mktcap = ind_mktcap.sum(axis=1)
+    ind_capweight = ind_mktcap.divide(total_mktcap, axis="rows")
     total_market_return = (ind_capweight * ind_return).sum(axis="columns")
     return total_market_return
                          
